8039dd8ae35a1fbf2fdfd0784c8162fbd88de484
Quentin Gaillardet <quentin.gaillardet01@gmail.com>
2023-04-05 11:00:36 +0200

    Add the MainActivity to show all the navigation

---
+++ app/src/main/java/fr/deuspheara/potterdbapp/ui/main/MainActivity.kt
@@ -0,0 +1,57 @@
+package fr.deuspheara.potterdbapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import fr.deuspheara.potterdbapp.R
+import fr.deuspheara.potterdbapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    private val navController : NavController
+        get() = (supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment).navController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeFragment -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.charactersFragment -> {
+                    navController.navigate(R.id.charactersFragment)
+                    true
+                }
+                R.id.moviesFragment -> {
+                    navController.navigate(R.id.moviesFragment)
+                    true
+                }
+                R.id.booksFragment -> {
+                    navController.navigate(R.id.booksFragment)
+                    true
+                }
+                R.id.favoriteFragment -> {
+                    navController.navigate(R.id.favoriteFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
